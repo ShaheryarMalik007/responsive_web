689f31f9d711ef6dde036440bbb08fb4016d3bcb
M Shaheryar Malik <sherrimalik07@gmail.com>
2022-06-12 20:24:27 +0500

    Add files via upload
    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_web/ui/home.dart';
+import 'package:flutter_app_web/utils/screen/screen_utils.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      color: Color(0xFF2c2f32),
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColorBrightness: Brightness.light,
+          accentColorBrightness: Brightness.light
+      ),
+      home: MyAppChild(),
+    );
+  }
+}
+
+class MyAppChild extends StatefulWidget {
+
+  @override
+  _MyAppChildState createState() => _MyAppChildState();
+}
+
+class _MyAppChildState extends State<MyAppChild> {
+  @override
+  Widget build(BuildContext context) {
+    // instantiating ScreenUtil singleton instance, as this will be used throughout
+    // the app to get screen size and other stuff
+    ScreenUtil.instance = ScreenUtil.getInstance()..init(context);
+    return HomePage();
+  }
+}
